7bd93a9afe6263e9466c420233717eabea59d148
Daniel Silva <rui.d.carvalhosilva@gmail.com>
2026-06-27 21:28:26 +0200

    Change camera movement to use Lerp instead of SmoothDamp
    Refactor LateUpdate method to use Lerp for smoother camera movement.

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -5,7 +5,6 @@ public class CameraMovement : MonoBehaviour
     public GameObject Obj_player; // * call player
     public float smoothCamSpd = 0.005f; // * speed at which it goes after the player
     private Vector3 offSet; // * distance between player and camera
-    private Vector3 velocity = Vector3.zero; // * add this line
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -23,9 +22,12 @@ public class CameraMovement : MonoBehaviour
 
     // ! late update is called after update functions so our player movement does not fight with the camera lerp. or else itd look shaky!!!! DONT change!!!1
 
-void LateUpdate()
-{
-    Vector3 targetPosition = Obj_player.transform.position + offSet;
-    transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothCamSpd);
-}
+        void LateUpdate()
+    {
+        Vector3 targetPosition = Obj_player.transform.position + offSet; // * we want to constantly be equal to the player's + the offset distance
+        
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothCamSpd); // * but instead of reaching at the same speed we lerp into it using the lerp built in along with the speed we defined
+
+        transform.position = smoothPosition; // * finally our(camera) position must be equal to that of smoothPos (we just defined paramaters above.)
+    }
 }
